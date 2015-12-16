124a01dd9c51865d3dde3bd51e995208feb78415
Nikola Begedin <begedinnikola@gmail.com>
2015-12-16 10:24:16 +0100

    Basic profile route with placeholder content

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import ENV from 'code-corps-ember/config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:oauth2'
+  authorizer: 'authorizer:oauth2',
+  host: ENV.API_BASE_URL
 });
---
+++ app/routes/profile.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model: function() {
+    var userId = this.get('session.session.content.authenticated.user_id');
+    return this.store.find('user', userId);
+  }
+});
