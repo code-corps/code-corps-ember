be0f25cfebb6ffa5aa492d024a4b4a055c007f62
Nikola Begedin <begedinnikola@gmail.com>
2016-01-11 10:12:16 +0100

    Add form input elements

--- tests/integration/components/signup-form-test.js
+++ tests/integration/components/signup-form-test.js
@@ -14,3 +14,16 @@ test('it renders', function(assert) {
 
   assert.equal(this.$('.signup-form').length, 1);
 });
+
+
+test('it renders required ui elements', function(assert) {
+  this.render(hbs`{{signup-form}}`);
+
+  let $component = this.$('.signup-form');
+  assert.equal($component.find('form').length, 1, 'The form renders');
+
+  let $form = $component.find('form');
+  assert.equal($form.find('[name=username]').length, 1, 'The username field renders');
+  assert.equal($form.find('[name=email]').length, 1, 'The email field renders');
+  assert.equal($form.find('[name=password]').length, 1, 'The password field renders');
+});
