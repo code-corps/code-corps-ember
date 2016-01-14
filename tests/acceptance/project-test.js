2d2292fb6dc228d44da2ee6018a6925b3dbb1b1a
Nikola Begedin <begedinnikola@gmail.com>
2016-01-14 15:33:51 +0100

    Acceptance test for post filtering

--- tests/acceptance/project-test.js
+++ tests/acceptance/project-test.js
@@ -13,7 +13,7 @@ module('Acceptance: Project', {
   }
 });
 
-test('It renders all the required ui elements', function(assert) {
+test('It renders all the required ui elements', (assert) => {
   assert.expect(3);
 
   let member = server.schema.member.create({ slug: 'test_user', modelType: 'user' });
@@ -33,5 +33,59 @@ test('It renders all the required ui elements', function(assert) {
     assert.equal(find('.project-details').length, 1, 'project-details component is rendered');
     assert.equal(find('.project-post-list').length, 1, 'project-post-list component is rendered');
     assert.equal(find('.project-post-list .post').length, 5, 'correct number of posts is rendered');
+  });
+});
+
+test('Filtering works', (assert) => {
+  assert.expect(5);
+
+  let member = server.schema.member.create({ slug: 'test_user', modelType: 'user' });
+  let user = member.createUser({ username: 'test_user' });
+  member.save();
+  let project = user.createProject({ slug: 'test_project' });
+  user.save();
+
+  for (let i = 0; i < 2; i++) {
+    project.createPost({postType: 'idea', title: 'An idea'});
+  }
+
+  for (let i = 0; i < 3; i++) {
+    project.createPost({postType: 'progress', title: 'A progress post'});
+  }
+
+  for (let i = 0; i < 4; i++) {
+    project.createPost({postType: 'task', title: 'A task' });
+  }
+
+  for (let i = 0; i < 5; i++) {
+    project.createPost({postType: 'issue', title: 'An issue'});
+  }
+
+  project.save();
+
+  visit('/test_user/test_project');
+
+  andThen(() => {
+    assert.equal(find('.project-post-list .post').length, 14, 'correct number of posts is rendered');
+    click('.filter-ideas');
+  });
+
+  andThen(() => {
+    assert.equal(find('.project-post-list .post.idea').length, 2, 'only ideas are rendered');
+    click('.filter-progress-posts');
+  });
+
+  andThen(() => {
+    assert.equal(find('.project-post-list .post.progress').length, 3, 'only progress posts are rendered');
+    click('.filter-tasks');
+  });
+
+  andThen(() => {
+    assert.equal(find('.project-post-list .post.task').length, 4, 'only tasks are rendered');
+    click('.filter-issues');
+  });
+
+  andThen(() => {
+    assert.equal(find('.project-post-list .post.issue').length, 5, 'only issues are rendered');
   });
 });
