5198e775c438d9665d7880ae4981cccc6114b3d5
Nikola Begedin <begedinnikola@gmail.com>
2016-01-20 15:39:39 +0100

    Rewrote post list component to avoid promise issues

--- app/components/project-post-list.js
+++ app/components/project-post-list.js
@@ -10,9 +10,46 @@ export default Ember.Component.extend({
       pageNumber: 1,
       postType: null
     });
+    this.send('reloadPosts');
   },
 
-  filteredPosts: Ember.computed('project', 'pageNumber', 'postType', function() {
+  reloadPosts() {
+    let filterParams = this.get('filterParams');
+
+    this.get('store').query('post', filterParams).then((posts) => {
+      this.setProperties({
+        filteredPosts: posts,
+        options: this._normalizeMeta(posts.get('meta'))
+      });
+    });
+  },
+
+
+  filterParams: Ember.computed('project.id', 'pageNumber', 'postType', function() {
+    let filterParams = {
+      projectId: this.get('project.id'),
+      page: { number: this.get('pageNumber') }
+    };
+
+    let postType = this.get('postType');
+
+    if (Ember.isPresent(postType)) {
+      filterParams.postType = postType;
+    }
+
+    return filterParams;
+  }),
+
+  _normalizeMeta(meta) {
+    return {
+      currentPage: meta.current_page,
+      pageSize: meta.page_size,
+      totalPages: meta.total_pages,
+      totalRecords: meta.total_records
+    };
+  },
+
+  /*filteredPosts: Ember.computed('project', 'pageNumber', 'postType', function() {
     let filterParams = {
       projectId: this.get('project.id'),
       page: {
@@ -39,18 +76,21 @@ export default Ember.Component.extend({
         totalRecords: meta.total_records
       };
     });
-  }),
+  }),*/
 
   actions: {
     filterBy(postType) {
-      this.setProperties({
-        'postType': postType,
-        'pageNumber': 1
-      });
+      this.setProperties({ postType: postType, pageNumber: 1 });
+      this.send('reloadPosts');
     },
 
     pageChanged(pageNumber) {
       this.set('pageNumber', pageNumber);
+      this.send('reloadPosts');
+    },
+
+    reloadPosts() {
+      this.reloadPosts();
     }
   }
 });
