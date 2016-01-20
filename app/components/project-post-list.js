c6bd379aac509e5a4815fdcbc6373d8e4afee3f1
Nikola Begedin <begedinnikola@gmail.com>
2016-01-20 15:40:15 +0100

    Cleanup of project-post-list component

--- app/components/project-post-list.js
+++ app/components/project-post-list.js
@@ -24,7 +24,6 @@ export default Ember.Component.extend({
     });
   },
 
-
   filterParams: Ember.computed('project.id', 'pageNumber', 'postType', function() {
     let filterParams = {
       projectId: this.get('project.id'),
@@ -48,35 +47,6 @@ export default Ember.Component.extend({
       totalRecords: meta.total_records
     };
   },
-
-  /*filteredPosts: Ember.computed('project', 'pageNumber', 'postType', function() {
-    let filterParams = {
-      projectId: this.get('project.id'),
-      page: {
-        number: this.get('pageNumber')
-      }
-    };
-
-    let postType = this.get('postType');
-
-    if (Ember.isPresent(postType)) {
-      filterParams.postType = postType;
-    }
-
-    return this.get('store').query('post', filterParams);
-  }),
-
-  options: Ember.computed('filteredPosts', function() {
-    return this.get('filteredPosts').then(function(posts) {
-      let meta = posts.get('meta');
-      return {
-        currentPage: meta.current_page,
-        pageSize: meta.page_size,
-        totalPages: meta.total_pages,
-        totalRecords: meta.total_records
-      };
-    });
-  }),*/
 
   actions: {
     filterBy(postType) {
