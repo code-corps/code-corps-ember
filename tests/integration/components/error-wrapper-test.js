8636aaa1d2b417f858bf3785622e73b412ff5aa3
Josh Smith <joshdotsmith@gmail.com>
2016-01-30 18:46:23 -0800

    Fix error page and add test for it

--- tests/integration/components/error-wrapper-test.js
+++ tests/integration/components/error-wrapper-test.js
@@ -11,7 +11,7 @@ test('it renders', function(assert) {
 });
 
 test('it renders all required elements for the 404 case', function(assert) {
-  assert.expect(5);
+  assert.expect(6);
 
   var model = {
     errors: [{
@@ -27,10 +27,11 @@ test('it renders all required elements for the 404 case', function(assert) {
   assert.equal(this.$('p:first').text().trim(), "We can't find the page you're looking for.", 'The body renders');
   assert.equal(this.$('a.button').text().trim(), "Go Home", 'The button renders');
   assert.equal($('html').attr('class'), "warning", 'The html element has the right class');
+  assert.notEqual($('html').css('background-color'), "rgba(0, 0, 0, 0)", 'The html element does not have a white background');
 });
 
 test('it renders all required elements for the general error case', function(assert) {
-  assert.expect(5);
+  assert.expect(6);
 
   var model = {
     errors: [{
@@ -46,4 +47,5 @@ test('it renders all required elements for the general error case', function(ass
   assert.equal(this.$('p:first').text().trim(), "Something went wrong. Try again and if the problem persists, please report your problem and mention what caused it.", 'The body renders');
   assert.equal(this.$('a.button').text().trim(), "Go Home", 'The button renders');
   assert.equal($('html').attr('class'), "danger", 'The html element has the right class');
+  assert.notEqual($('html').css('background-color'), "rgba(0, 0, 0, 0)", 'The html element does not have a white background');
 });
