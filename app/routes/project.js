2a855d7ad178ec7c6e48a7e8c93a305105a2f5a0
Nikola Begedin <begedinnikola@gmail.com>
2016-02-05 16:22:27 +0100

    Fixed issue with login redirection to bad url from project.posts.new

--- app/routes/project.js
+++ app/routes/project.js
@@ -6,5 +6,16 @@ export default Ember.Route.extend({
       slug: params.project_slug,
       sluggedRouteSlug: params.slugged_route_slug
     });
+  },
+
+  serialize: function(model) {
+    if (model) {
+      return {
+        slugged_route_slug: model.get('organization.slug'),
+        project_slug: model.get('slug')
+      };
+    } else {
+      return this._super(...arguments);
+    }
   }
 });
