8f03113699481cc1bdf39b52fde2df58ec4107d5
Josh Smith <joshdotsmith@gmail.com>
2016-02-25 10:12:25 -0800

    Reorder settings and more route changes

--- app/router.js
+++ app/router.js
@@ -8,22 +8,24 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('login');
   this.route('signup');
-  this.route('profile', { path: '/settings/profile' });
+  this.route('settings', function() {
+    this.route('profile');
+  });
   this.route('slugged-route', { path: '/:slugged_route_slug' });
   this.route('projects-list', { path: '/projects'});
   this.route('projects', { path: '/:slugged_route_slug/projects'});
 
-  this.route('organizations', { path: '/organizations' }, function() {
+  this.route('organizations', function() {
     this.route('settings', { path: '/:slugged_route_slug/settings'}, function() {
       this.route('profile');
     });
   });
 
   this.route('project', { path: '/:slugged_route_slug/:project_slug' }, function() {
-    this.route('settings', { path: '/settings' }, function() {
+    this.route('settings', function() {
       this.route('profile');
     });
-    this.route('posts', { path: '/posts' }, function() {
+    this.route('posts', function() {
       this.route('new');
       this.route('post', { path: '/:number' });
     });
