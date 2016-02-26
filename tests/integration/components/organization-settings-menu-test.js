f183e83d4bfaa98174e38754eb0808a918d7a053
Josh Smith <joshdotsmith@gmail.com>
2016-02-25 18:14:32 -0800

    Add organization settings menu test

--- tests/integration/components/organization-settings-menu-test.js
+++ tests/integration/components/organization-settings-menu-test.js
@@ -5,20 +5,12 @@ moduleForComponent('organization-settings-menu', 'Integration | Component | orga
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
+test('it renders with all its required elements', function(assert) {
+  assert.expect(3);
 
   this.render(hbs`{{organization-settings-menu}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#organization-settings-menu}}
-      template block text
-    {{/organization-settings-menu}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.organization-settings-menu').length, 1, 'The component itself renders');
+  assert.equal(this.$('li').length, 1, 'All the menu items render');
+  assert.equal(this.$('li:eq(0) a').text().trim(), "Organization Profile", 'The profile link render');
 });
