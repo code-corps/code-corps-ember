edb31c928487ab552db6b35c8f109e0bdcfb429b
Josh Smith <joshdotsmith@gmail.com>
2016-02-25 19:40:35 -0800

    Fix more tests

--- tests/unit/models/organization-test.js
+++ tests/unit/models/organization-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('organization', 'Unit | Model | organization', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:project', 'model:user']
 });
 
 test('it exists', function(assert) {
