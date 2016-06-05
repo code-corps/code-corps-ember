773bf6360e8029f51f4911efd00caf804acda650
Josh Smith <joshdotsmith@gmail.com>
2016-06-05 12:55:03 -0700

    Rewrite flash messages and make loading of category items snappier
    Add loading spinners to category buttons and get tests passing

--- app/components/category-item.js
+++ app/components/category-item.js
@@ -4,7 +4,10 @@ export default Ember.Component.extend({
   classNames: ['category-item'],
   classNameBindings: ['selected'],
 
+  isLoading: false,
+
   currentUser: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
   userCategories: Ember.inject.service(),
 
   user: Ember.computed.alias('currentUser.user'),
@@ -15,4 +18,37 @@ export default Ember.Component.extend({
     let userCategories = this.get('userCategories');
     return userCategories.hasCategory(category);
   }),
+
+  actions: {
+    addCategory(category) {
+      this.set('isLoading', true);
+      this.addCategory(category).catch(() => {
+        let message = `An error occurred trying to add ${category.get('name')}.`;
+        this._flashError(message);
+      }).finally(() => {
+        this.set('isLoading', false);
+      });
+    },
+    removeCategory(category) {
+      this.set('isLoading', true);
+      this.removeCategory(category).catch(() => {
+        let message = `An error occurred trying to remove ${category.get('name')}.`;
+        this._flashError(message);
+      }).finally(() => {
+        this.set('isLoading', false);
+      });
+    },
+  },
+
+  _flashError(message) {
+    const flashMessages = this.get('flashMessages');
+    flashMessages.clearMessages();
+    flashMessages.add({
+      message: message,
+      type: 'danger',
+      fixed: true,
+      sticky: false,
+      timeout: 5000,
+    });
+  },
 });
--- tests/integration/components/category-item-test.js
+++ tests/integration/components/category-item-test.js
@@ -1,6 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
 
 moduleForComponent('category-item', 'Integration | Component | category item', {
   integration: true,
@@ -21,19 +22,31 @@ let mockUserCategory = Ember.Object.create({
   userId: 1,
 });
 
+let unselectedCategory = Ember.Object.create({
+  id: 1,
+  name: 'Technology',
+  slug: 'technology',
+  description: 'You want to help technology.',
+});
+
+let selectedCategory = Ember.Object.create({
+  id: 2,
+  name: 'Society',
+  slug: 'society',
+  description: 'You want to help society.',
+});
+
 test('it works for selecting unselected categories', function(assert) {
   assert.expect(6);
 
-  let category = {
-      id: 1,
-      name: 'Technology',
-      slug: 'technology',
-      description: 'You want to help technology.',
-  };
-
-  this.set('category', category);
+  this.set('category', unselectedCategory);
   this.set('addCategory', (clickedCategory) => {
-    assert.deepEqual(clickedCategory, category);
+    assert.deepEqual(clickedCategory, unselectedCategory);
+    return new Ember.RSVP.Promise((fulfill) => {
+      Ember.run.next(() => {
+        fulfill();
+      });
+    });
   });
   this.render(hbs`{{category-item category=category addCategory=(action addCategory)}}`);
 
@@ -49,22 +62,136 @@ test('it works for selecting unselected categories', function(assert) {
 test('it works for removing selected categories', function(assert) {
   assert.expect(4);
 
-  let category = {
-      id: 2,
-      name: 'Society',
-      slug: 'society',
-      description: 'You want to help society.',
-  };
-
-  this.set('category', category);
+  this.set('category', selectedCategory);
   this.set('removeCategory', (clickedCategory) => {
-    assert.deepEqual(clickedCategory, category);
+    assert.deepEqual(clickedCategory, selectedCategory);
+    return new Ember.RSVP.Promise((fulfill) => {
+      Ember.run.next(() => {
+        fulfill();
+      });
+    });
   });
   this.render(hbs`{{category-item category=category removeCategory=(action removeCategory)}}`);
 
   assert.equal(this.$('.category-icon').hasClass('selected'), true);
   assert.equal(this.$('p').hasClass('selected'), true);
   assert.equal(this.$('button').text().trim(), 'Society');
+
+  this.$('button').click();
+});
+
+test('it creates a flash message on an error when adding', function(assert) {
+  assert.expect(7);
+
+  this.set('category', unselectedCategory);
+  this.set('addCategory', (clickedCategory) => {
+    assert.deepEqual(clickedCategory, unselectedCategory);
+    return new Ember.RSVP.Promise((fulfill, reject) => {
+      Ember.run.next(() => {
+        reject();
+      });
+    });
+  });
+
+  let mockFlashMessages = Ember.Service.extend({
+    clearMessages() {
+      assert.ok(true);
+    },
+    add(object) {
+      assert.ok(object.message.indexOf(unselectedCategory.name) !== -1);
+      assert.equal(object.type, 'danger');
+      assert.equal(object.fixed, true);
+      assert.equal(object.sticky, false);
+      assert.equal(object.timeout, 5000);
+    }
+  });
+  this.container.registry.register('service:flash-messages', mockFlashMessages);
+
+  this.render(hbs`{{category-item category=category addCategory=(action addCategory)}}`);
+
+  this.$('button').click();
+});
+
+test('it creates a flash message on an error when removing', function(assert) {
+  assert.expect(7);
+
+  this.set('category', selectedCategory);
+  this.set('removeCategory', (clickedCategory) => {
+    assert.deepEqual(clickedCategory, selectedCategory);
+    return new Ember.RSVP.Promise((fulfill, reject) => {
+      Ember.run.next(() => {
+        reject();
+      });
+    });
+  });
+
+  let mockFlashMessages = Ember.Service.extend({
+    clearMessages() {
+      assert.ok(true);
+    },
+    add(object) {
+      assert.ok(object.message.indexOf(selectedCategory.name) !== -1);
+      assert.equal(object.type, 'danger');
+      assert.equal(object.fixed, true);
+      assert.equal(object.sticky, false);
+      assert.equal(object.timeout, 5000);
+    }
+  });
+  this.container.registry.register('service:flash-messages', mockFlashMessages);
+
+  this.render(hbs`{{category-item category=category removeCategory=(action removeCategory)}}`);
+
+  this.$('button').click();
+});
+
+test('it sets and unsets loading state when adding', function(assert) {
+  let done = assert.async();
+
+  assert.expect(4);
+
+  this.set('category', unselectedCategory);
+  this.set('addCategory', (clickedCategory) => {
+    assert.deepEqual(clickedCategory, unselectedCategory);
+    return new Ember.RSVP.Promise((fulfill) => {
+      Ember.run.next(() => {
+        fulfill();
+        assert.ok(this.$('span').hasClass('button-spinner'));
+        assert.notOk(this.$('span').hasClass('check-area'));
+        wait().then(() => {
+          assert.notOk(this.$('span').hasClass('button-spinner'));
+          done();
+        });
+      });
+    });
+  });
+
+  this.render(hbs`{{category-item category=category addCategory=(action addCategory)}}`);
+
+  this.$('button').click();
+});
+
+test('it sets and unsets loading state when removing', function(assert) {
+  let done = assert.async();
+
+  assert.expect(4);
+
+  this.set('category', selectedCategory);
+  this.set('removeCategory', (clickedCategory) => {
+    assert.deepEqual(clickedCategory, selectedCategory);
+    return new Ember.RSVP.Promise((fulfill) => {
+      Ember.run.next(() => {
+        fulfill();
+        assert.ok(this.$('span').hasClass('button-spinner'));
+        assert.notOk(this.$('span').hasClass('check-area'));
+        wait().then(() => {
+          assert.notOk(this.$('span').hasClass('button-spinner'));
+          done();
+        });
+      });
+    });
+  });
+
+  this.render(hbs`{{category-item category=category removeCategory=(action removeCategory)}}`);
 
   this.$('button').click();
 });
