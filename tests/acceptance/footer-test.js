d70ef42025bcb9feea96dfba1d3662fe72e9e083
Josh Smith <joshdotsmith@gmail.com>
2016-06-17 08:58:26 -0700

    Add about page
    Add to the footer and to team template

--- tests/acceptance/footer-test.js
+++ tests/acceptance/footer-test.js
@@ -7,6 +7,14 @@ test('can visit links from footer', function(assert) {
   visit('/');
 
   andThen(function() {
+    click('.site-footer ul > li:eq(1) li:eq(0) a');
+  });
+
+  andThen(function() {
+    assert.equal(currentURL(), '/about');
+  });
+
+  andThen(function() {
     click('.site-footer ul > li:eq(1) li:eq(1) a');
   });
 
