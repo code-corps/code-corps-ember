e36aed23fb9a34ca67e93f8346323bdd59908fad
Josh Smith <joshdotsmith@gmail.com>
2016-06-21 00:24:45 -0700

    Update contributors
    Fix number of people in team

--- tests/acceptance/team-test.js
+++ tests/acceptance/team-test.js
@@ -10,6 +10,6 @@ test('visiting /team', function(assert) {
     assert.equal(currentURL(), '/team');
     assert.equal(find('.company h2').text().trim(), 'Our Team');
     assert.equal(find('.company li').length, 2);
-    assert.equal(find('.contributors li').length, 13);
+    assert.equal(find('.contributors li').length, 15);
   });
 });
