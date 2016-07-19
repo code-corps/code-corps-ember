a4e98db3204ce447327049e690e5347903c523a3
wenincode <twendlan@ualberta.ca>
2016-07-18 23:13:00 -0600

    add the rest of the post component docs

--- app/components/post-comment-list.js
+++ app/components/post-comment-list.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
+/**
+  The post-comment-list component composes the list of comments for a post.
+
+  ## default usage
+
+  ```handlebars
+  {{post-comment-list comments=comments}}
+  ```
+
+  @class post-comment-list
+  @module Component
+  @extends Ember.Component
+ */
 export default Ember.Component.extend({
   classNames: ['post-comment-list']
 });
