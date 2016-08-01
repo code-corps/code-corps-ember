6ec1785d52eeb79e8a8ea630a96418eb6ca70f9e
Tyler Wendlandt <twendlan@ualberta.ca>
2016-08-01 18:42:25 -0400

    add navigation-menu component documentation (#352)

--- app/components/navigation-menu.js
+++ app/components/navigation-menu.js
@@ -2,11 +2,43 @@ import Ember from 'ember';
 
 const { service } = Ember.inject;
 
+/**
+  `navigation-menu` composes the top navigation-menu.
+
+  ## default usage
+
+  ```handlebars
+  {{navigation-menu}}
+  ```
+
+  @class navigation-menu
+  @module Component
+  @extends Ember.Component
+ */
 export default Ember.Component.extend({
   classNames: ['menu', 'container'],
 
+  /**
+    @property currentUser
+    @type Ember.Service
+   */
   currentUser: service(),
+
+  /**
+    @property navigationMenu
+    @type Ember.Service
+   */
   navigationMenu: service(),
+
+  /**
+    @property onboarding
+    @type Ember.Service
+   */
   onboarding: service(),
+
+  /**
+    @property session
+    @type Ember.Service
+   */
   session: service(),
 });
