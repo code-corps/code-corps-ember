85b3e6f1fba6e295d7a3aaef0c935384af9fe448
Tyler Wendlandt <twendlan@ualberta.ca>
2016-08-14 22:08:29 -0600

    add documentation for progress-bar-container (#366)

--- app/components/progress-bar-container.js
+++ app/components/progress-bar-container.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  Component,
+} = Ember;
+
+/**
+  `progress-bar-container` is the container for a progress-bar.
+
+  ## default usage
+
+  ```Handlebars
+  {{progress-bar-container percentage=percentage}}
+  ```
+
+  @class progress-bar-container
+  @module Component
+  @extends Ember.Component
+*/
+export default Component.extend({
   classNames: ['progress-bar-container'],
 });
