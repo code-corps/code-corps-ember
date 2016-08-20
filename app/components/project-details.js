38b245be06f0b5bc51519c1dfccb62ce4e4f1890
Riley Taylor <rileyhtaylor@outlook.com>
2016-08-20 12:20:11 -0700

    Adds documentation for the project-details component

--- app/components/project-details.js
+++ app/components/project-details.js
@@ -1,16 +1,53 @@
 import Ember from 'ember';
 
+/**
+  Displays information about the project and allows a user to join the project
+
+  ## default usage
+  ```handlebars
+  {{project-details project=project}}
+  ```
+
+  @module Components
+  @extends Ember.Component
+  @class project-details
+ */
+
 export default Ember.Component.extend({
   classNames: ['project-details'],
   classNameBindings: ['expanded'],
   expanded: false,
 
+  /**
+    @property store
+    @type Ember.service
+   */
   store: Ember.inject.service(),
+
+  /**
+    @property session
+    @type Ember.service
+   */
   session: Ember.inject.service(),
+
+  /**
+    @property credentials
+    @type Ember.service
+   */
   credentials: Ember.inject.service(),
+
+  /**
+    @property currentUser
+    @type Ember.service
+   */
   currentUser: Ember.inject.service(),
 
   actions: {
+    /**
+      Action that allows a user to join a project.
+
+      @method joinProject
+     */
     joinProject() {
       let currentUser = this.get('currentUser.user');
 
