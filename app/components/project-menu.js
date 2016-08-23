9971b1278a9bfc571520fa18e82f8c2fbef1922b
Riley <rileyhtaylor@outlook.com>
2016-08-22 20:52:06 -0700

    Adds documentation for project-menu

--- app/components/project-menu.js
+++ app/components/project-menu.js
@@ -1,9 +1,36 @@
 import Ember from 'ember';
 
+/**
+  `project-menu` allows navigation within project functions. Users who
+  manage an organization can also view contributors and project settings through
+  additional menu items.
+
+  ## Default usage
+  ```Handlebars
+    {{project-menu project=model}}
+  ```
+
+  @module Component
+  @extends Ember.Component
+  @class project-menu
+ */
 export default Ember.Component.extend({
   classNames: ['project-menu', 'horizontal-menu'],
   tagName: 'ul',
 
+  /**
+    Retreives user credentials to show more content for project managers.
+
+    @property credentials
+    @type Ember.Service
+   */
   credentials: Ember.inject.service(),
+
+  /**
+    Service that provides user authentication information.
+
+    @property session
+    @type Ember.Service
+   */
   session: Ember.inject.service(),
 });
