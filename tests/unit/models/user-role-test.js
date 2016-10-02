e403c565ff3a7221087b4b4536c99a27150c2698
wenincode <twendlan@ualberta.ca>
2016-10-02 12:51:35 -0600

    use relationship test helpers

--- tests/unit/models/user-role-test.js
+++ tests/unit/models/user-role-test.js
@@ -1,9 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
-import Ember from 'ember';
-
-const {
-  get,
-} = Ember;
+import { testForBelongsTo } from '../../helpers/relationship';
 
 moduleForModel('user-role', 'Unit | Model | user role', {
   // Specify the other units that are required for this test.
@@ -16,22 +12,5 @@ test('it exists', function(assert) {
   assert.ok(!!model);
 });
 
-test('it should belong to a role', function(assert) {
-  assert.expect(2);
-
-  const userRole = this.store().modelFor('user-role');
-  const relationship = get(userRole, 'relationshipsByName').get('role');
-
-  assert.equal(relationship.key, 'role', 'has relationship with role');
-  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
-});
-
-test('it should belong to a user', function(assert) {
-  assert.expect(2);
-
-  const userRole = this.store().modelFor('user-role');
-  const relationship = get(userRole, 'relationshipsByName').get('user');
-
-  assert.equal(relationship.key, 'user', 'has relationship with user');
-  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
-});
+testForBelongsTo('user-role', 'role');
+testForBelongsTo('user-role', 'user');
