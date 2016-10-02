0b3402138f0dbd54cb689046d807f994148dd39d
wenincode <twendlan@ualberta.ca>
2016-10-01 22:51:33 -0600

    update category unit model tests

--- tests/unit/models/category-test.js
+++ tests/unit/models/category-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { testForHasMany } from '../../helpers/relationship';
 
 moduleForModel('category', 'Unit | Model | category', {
   // Specify the other units that are required for this test.
@@ -10,3 +11,17 @@ test('it exists', function(assert) {
   // let store = this.store();
   assert.ok(!!model);
 });
+
+test('it should have all its attributes', function(assert) {
+  assert.expect(3);
+
+  const category = this.subject();
+  const attributes = Object.keys(category.toJSON());
+
+  assert.ok(attributes.includes('description'), 'should have description attribute');
+  assert.ok(attributes.includes('name'), 'should have name attribute');
+  assert.ok(attributes.includes('slug'), 'should have slug attribute');
+});
+
+testForHasMany('category', 'projectCategories');
+testForHasMany('category', 'userCategories');
