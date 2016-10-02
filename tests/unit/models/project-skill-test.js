e075614d6bb4b8de31621a9ccec5a3e6764d76e0
wenincode <twendlan@ualberta.ca>
2016-10-01 19:06:21 -0600

    update project-skill model test

--- tests/unit/models/project-skill-test.js
+++ tests/unit/models/project-skill-test.js
@@ -1,4 +1,9 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get,
+} = Ember;
 
 moduleForModel('project-skill', 'Unit | Model | project skill', {
   // Specify the other units that are required for this test.
@@ -9,4 +14,24 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should belong to a project', function(assert) {
+  assert.expect(2);
+
+  const projectSkill = this.store().modelFor('project-skill');
+  const relationship = get(projectSkill, 'relationshipsByName').get('project');
+
+  assert.equal(relationship.key, 'project', 'has relationship with project');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
+});
+
+test('should belong to a skill', function(assert) {
+  assert.expect(2);
+
+  const projectSkill = this.store().modelFor('project-skill');
+  const relationship = get(projectSkill, 'relationshipsByName').get('skill');
+
+  assert.equal(relationship.key, 'skill', 'has relationship with skill');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
 });
