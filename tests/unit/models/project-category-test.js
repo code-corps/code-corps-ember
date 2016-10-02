c8840c1f8454b52fc427fb7508f894abeba93eaf
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-01 22:29:18 -0600

    update project-category model tests (#542)

--- tests/unit/models/project-category-test.js
+++ tests/unit/models/project-category-test.js
@@ -1,4 +1,9 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get,
+} = Ember;
 
 moduleForModel('project-category', 'Unit | Model | project category', {
   // Specify the other units that are required for this test.
@@ -9,4 +14,24 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('it should belong to a category', function(assert) {
+  assert.expect(2);
+
+  const projectCategory = this.store().modelFor('project-category');
+  const relationship = get(projectCategory, 'relationshipsByName').get('category');
+
+  assert.equal(relationship.key, 'category', 'has relationship with category');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
+});
+
+test('it should belong to a project', function(assert) {
+  assert.expect(2);
+
+  const projectCategory = this.store().modelFor('project-category');
+  const relationship = get(projectCategory, 'relationshipsByName').get('project');
+
+  assert.equal(relationship.key, 'project', 'has relationship with project');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
 });
