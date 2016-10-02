c5548d3faf53b869c7f958a97a5895b5635567e0
wenincode <twendlan@ualberta.ca>
2016-10-01 22:40:08 -0600

    update comment unit model tests

--- tests/unit/models/comment-test.js
+++ tests/unit/models/comment-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { testForBelongsTo, testForHasMany } from '../../helpers/relationship';
 
 moduleForModel('comment', 'Unit | Model | comment', {
   // Specify the other units that are required for this test.
@@ -14,6 +15,21 @@ test('it exists', function(assert) {
   // let store = this.store();
   assert.ok(!!model);
 });
+
+test('it has all of its attributes', function(assert) {
+  assert.expect(3);
+
+  const comment = this.subject();
+  const attributes = Object.keys(comment.toJSON());
+
+  assert.ok(attributes.includes('body'), 'should have body attribute');
+  assert.ok(attributes.includes('insertedAt'), 'should have insertedAt attribute');
+  assert.ok(attributes.includes('markdown'), 'should have markdown attribute');
+});
+
+testForBelongsTo('comment', 'task');
+testForBelongsTo('comment', 'user');
+testForHasMany('comment', 'commentUserMentions');
 
 test('it correctly identifies code in the body', function(assert) {
   assert.expect(1);
