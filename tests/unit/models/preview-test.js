13c01c50d37c8da3da4a1bc555a32b5595c02d3e
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-01 22:39:20 -0600

    update preview unit model tests (#543)

--- tests/unit/models/preview-test.js
+++ tests/unit/models/preview-test.js
@@ -1,4 +1,9 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get,
+} = Ember;
 
 moduleForModel('preview', 'Unit | Model | preview', {
   // Specify the other units that are required for this test.
@@ -9,4 +14,24 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should belong to a user', function(assert) {
+  assert.expect(2);
+
+  const preview = this.store().modelFor('preview');
+  const relationship = get(preview, 'relationshipsByName').get('user');
+
+  assert.equal(relationship.key, 'user', 'has relationship with user');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
+});
+
+test('should have many preview-user-mention', function(assert) {
+  assert.expect(2);
+
+  const preview = this.store().modelFor('preview');
+  const relationship = get(preview, 'relationshipsByName').get('previewUserMentions');
+
+  assert.equal(relationship.key, 'previewUserMentions', 'has relationship with preview-user-mention');
+  assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
