bda7f9fb8b4dda170ad7c6445cf004b29a07756d
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-02 14:30:17 -0600

    update organization-membership unit model tests (#544)

--- tests/unit/models/organization-membership-test.js
+++ tests/unit/models/organization-membership-test.js
@@ -1,4 +1,12 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { testForBelongsTo } from '../../helpers/relationship';
+import Ember from 'ember';
+
+const {
+  get,
+  set,
+  run,
+} = Ember;
 
 moduleForModel('organization-membership', 'Unit | Model | organization membership', {
   // Specify the other units that are required for this test.
@@ -9,4 +17,60 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('it should have all of its attributes', function(assert) {
+  assert.expect(1);
+
+  let organizationMembership = this.subject();
+  let attributes = Object.keys(organizationMembership.toJSON());
+
+  assert.ok(attributes.includes('role'), 'should have the role attribute');
+});
+
+testForBelongsTo('organization-membership', 'member');
+testForBelongsTo('organization-membership', 'organization');
+
+test('isAdmin should compute if the role is admin', function(assert) {
+  assert.expect(2);
+
+  let organizationMembership = this.subject({ role: 'contributor' });
+  assert.notOk(get(organizationMembership, 'isAdmin'), 'isAdmin should be false');
+  run(() => {
+    set(organizationMembership, 'role', 'admin');
+  });
+  assert.ok(get(organizationMembership, 'isAdmin'), 'isAdmin should be true');
+});
+
+test('isContributor should compute if the role is contributor', function(assert) {
+  assert.expect(2);
+
+  let organizationMembership = this.subject({ role: 'admin' });
+  assert.notOk(get(organizationMembership, 'isContributor'), 'isContributor should be false');
+  run(() => {
+    set(organizationMembership, 'role', 'contributor');
+  });
+  assert.ok(get(organizationMembership, 'isContributor'), 'isContributor should be true');
+});
+
+test('isOwner should compute if the role is owner', function(assert) {
+  assert.expect(2);
+
+  let organizationMembership = this.subject({ role: 'contributor' });
+  assert.notOk(get(organizationMembership, 'isOwner'), 'isOwner should be false');
+  run(() => {
+    set(organizationMembership, 'role', 'owner');
+  });
+  assert.ok(get(organizationMembership, 'isOwner'), 'isOwner should be true');
+});
+
+test('isPending should compute if the role is pending', function(assert) {
+  assert.expect(2);
+
+  let organizationMembership = this.subject({ role: 'contributor' });
+  assert.notOk(get(organizationMembership, 'isPending'), 'isPending should be false');
+  run(() => {
+    set(organizationMembership, 'role', 'pending');
+  });
+  assert.ok(get(organizationMembership, 'isPending'), 'isPending should be true');
 });
