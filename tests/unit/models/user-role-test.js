5b17077aef982d07a0017fe3c994af8bfc06ae4b
wenincode <twendlan@ualberta.ca>
2016-10-01 23:24:21 -0600

    update user-role unit model tests

--- tests/unit/models/user-role-test.js
+++ tests/unit/models/user-role-test.js
@@ -1,4 +1,9 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get,
+} = Ember;
 
 moduleForModel('user-role', 'Unit | Model | user role', {
   // Specify the other units that are required for this test.
@@ -9,4 +14,24 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('it should belong to a role', function(assert) {
+  assert.expect(2);
+
+  const userRole = this.store().modelFor('user-role');
+  const relationship = get(userRole, 'relationshipsByName').get('role');
+
+  assert.equal(relationship.key, 'role', 'has relationship with role');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
+});
+
+test('it should belong to a user', function(assert) {
+  assert.expect(2);
+
+  const userRole = this.store().modelFor('user-role');
+  const relationship = get(userRole, 'relationshipsByName').get('user');
+
+  assert.equal(relationship.key, 'user', 'has relationship with user');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
 });
