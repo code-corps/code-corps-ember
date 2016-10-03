69ac8a5a477facdec408a546fd89c2445eb58764
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-02 23:06:05 -0600

    update slugged-route model unit tests (#551)

--- tests/unit/models/slugged-route-test.js
+++ tests/unit/models/slugged-route-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { testForBelongsTo } from '../../helpers/relationship';
 
 moduleForModel('slugged-route', 'Unit | Model | slugged-route', {
   // Specify the other units that are required for this test.
@@ -10,3 +11,17 @@ test('it exists', function(assert) {
   // let store = this.store();
   assert.ok(!!model);
 });
+
+test('should have all of its attributes', function(assert) {
+  assert.expect(1);
+
+  let sluggedRoute = this.subject();
+  let attributes = Object.keys(sluggedRoute.toJSON());
+
+  assert.ok(attributes.includes('slug'), 'should have the slug attribute');
+});
+
+// slugged-route may belong to either a user or organization, but not both
+// at the same time.
+testForBelongsTo('slugged-route', 'organization');
+testForBelongsTo('slugged-route', 'user');
