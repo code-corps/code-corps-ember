fc1c9e089b50fa14bd375d085dcee74d29693f0c
Filipe Bragança <filipe@primeiroestilo.com.br>
2016-10-03 18:06:34 -0700

    Update Project Model Test and create new has-attribute helper

---
+++ tests/helpers/has-attributes.js
@@ -0,0 +1,23 @@
+import QUnit from 'qunit';
+/*
+  This assertion will compare 2 arrays of attributes.
+  It first sorts both arrays and then compares each element.
+
+  @method hasAttributes
+  @param {Array} actualAttributes
+  @param {Array} expectedAttributes
+*/
+function compareArrays(actualAttributes, expectedAttributes) {
+  expectedAttributes.sort();
+
+  return actualAttributes.sort().every(function(element, index) {
+    return element === expectedAttributes[index]; 
+  });
+}
+
+QUnit.assert.hasAttributes = function(actualAttributes, expectedAttributes) {
+  this.expect(2);
+
+  this.ok(actualAttributes.length === expectedAttributes.length, `should have ${expectedAttributes.length} attributes`);
+  this.ok(compareArrays(actualAttributes, expectedAttributes), 'should have the expected attributes');
+};
