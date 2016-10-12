999ce09140a7f4a09d647dc47f96cf4aed174e59
Riley <rileyhtaylor@outlook.com>
2016-10-12 11:23:04 -0700

    configure ember-try to use ember exam for testing in CI

---
+++ config/ember-try.js
@@ -0,0 +1,44 @@
+module.exports = {
+  command: "ember exam --split=3 --weighted --parallel",
+  scenarios: [
+    {
+      name: 'default',
+      bower: {
+        dependencies: { }
+      }
+    },
+    {
+      name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      }
+    }
+  ]
+};
