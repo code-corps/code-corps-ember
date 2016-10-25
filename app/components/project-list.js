2b36e6b95be4f5a669313cdb451653a7d9d4a9f1
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-25 08:35:32 -0600

    add documentation for the project-list component (#642)

--- app/components/project-list.js
+++ app/components/project-list.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 const { Component } = Ember;
 
+/**
+  `project-list` presents a list of projects.
+
+  ## default usage
+
+  ```handlebars
+  {{project-list projects=projects}}
+  ```
+
+  @class project-list
+  @module Component
+  @extends Ember.Component
+ */
 export default Component.extend({
   classNames: ['project-list']
 });
