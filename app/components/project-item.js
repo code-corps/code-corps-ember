e7f342aa04dccb2306cf3b40ac2c128c6d63e314
Tyler Wendlandt <twendlan@ualberta.ca>
2016-10-25 08:28:04 -0600

    add documentation for project-item component (#643)

--- app/components/project-item.js
+++ app/components/project-item.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 
 const { Component } = Ember;
 
+/**
+  `project-item` presents a project with the icon, title and description. Each
+  `project-item` links to the corresponding project.
+
+  ## default usage
+
+  ```handlebars
+  {{project-item project=project}}
+  ```
+
+  @class project-item
+  @module Component
+  @extends Ember.Component
+ */
 export default Component.extend({
   classNames: ['project-item']
 });
