4b41d5fe94d3e480b88e50019f9a5ac99d72aeeb
Eric Mosher <emoshmosh@gmail.com>
2016-10-30 11:24:23 -0400

    Add docs to a few components

--- app/components/user-settings-form.js
+++ app/components/user-settings-form.js
@@ -6,12 +6,32 @@ const {
   inject: { service }
 } = Ember;
 
+/**
+  `user-settings-form` composes the form for changing a user's settings
+
+  ```handlebars
+  {{user-settings-form user=model}}
+  ```
+
+  @class user-settings-form
+  @module Component
+  @extends Ember.Component
+ */
 export default Component.extend({
   classNames: ['user-settings-form'],
 
+  /**
+    @property flashMessages
+    @type Ember.Service
+   */
   flashMessages: service(),
 
   actions: {
+    /**
+      Action to save the user's settings.
+
+      @method save
+     */
     save() {
       let flashMessages = get(this, 'flashMessages');
 
