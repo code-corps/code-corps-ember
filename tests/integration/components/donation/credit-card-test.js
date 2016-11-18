04e7ba963c64b63aa71f94f0bef7e1e935740d57
wenincode <twendlan@ualberta.ca>
2016-11-17 23:25:32 -0700

    add cancel button to add-card form
    update tests for cancel add-card link

--- tests/integration/components/donation/credit-card-test.js
+++ tests/integration/components/donation/credit-card-test.js
@@ -15,14 +15,26 @@ const {
   run
 } = Ember;
 
-let setHandler = function(context, submitHandler = K) {
-  context.set('submitHandler', submitHandler);
+let setHandler = function(context, handlerName, handler = K) {
+  context.set(handlerName, handler);
+};
+
+let getFreshDates = function() {
+  let date = new Date();
+  let currentMonth = `0${date.getMonth() + 1}`.slice(-2);
+  let currentYear = date.getFullYear();
+
+  return {
+    month: currentMonth,
+    year: currentYear
+  };
 };
 
 moduleForComponent('donation/credit-card', 'Integration | Component | donation/credit card', {
   integration: true,
   beforeEach() {
-    setHandler(this);
+    setHandler(this, 'submitHandler');
+    setHandler(this, 'cancelHandler');
     page.setContext(this);
   },
   afterEach() {
@@ -54,7 +66,7 @@ test('it sends submit with credit card fields when button is clicked', function(
     });
   };
 
-  setHandler(this, submitHandler);
+  setHandler(this, 'submitHandler', submitHandler);
 
   stubService(this, 'stripe', {
     card: {
@@ -64,14 +76,62 @@ test('it sends submit with credit card fields when button is clicked', function(
     }
   });
 
-  page.render(hbs`{{donation/credit-card canDonate=canDonate submit=submitHandler}}`)
-      .cardNumber(expectedProps.cardNumber)
-      .cardMonth(expectedProps.month)
-      .cardYear(expectedProps.year)
-      .cardCVC(expectedProps.cvc)
-      .clickSubmit();
+  page.render(hbs`{{donation/credit-card canDonate=canDonate submit=submitHandler}}`);
+  page.cardNumber.fillIn(expectedProps.cardNumber);
+  page.cardMonth.selectOption(expectedProps.month);
+  page.cardCVC.fillIn(expectedProps.cvc);
+  page.cardYear.selectOption(expectedProps.year);
+  page.clickSubmit();
 
   wait().then(() => {
     done();
   });
+});
+
+test('cancel link should show if canCancel is true', function(assert) {
+  assert.expect(1);
+  page.render(hbs`{{donation/credit-card canCancel=true}}`);
+  assert.ok(page.isCancelVisible, 'cancel link should be visible');
+});
+
+test('cancel link should not show if canCancel is false', function(assert) {
+  assert.expect(1);
+  page.render(hbs`{{donation/credit-card canCancel=false}}`);
+  assert.notOk(page.isCancelVisible, 'cancel link should not be visible');
+});
+
+test('cancel clears the form fields and sends the proper action', function(assert) {
+  assert.expect(9);
+
+  let cancelHandler = function() {
+    assert.ok(true, 'credit-cards component should call this from the cancel action');
+  };
+
+  let testInput = {
+    cardNumber: '4242 4242 4242 4242',
+    cardMonth: '12',
+    cardYear: '2016',
+    cardCVC: '422'
+  };
+
+  setHandler(this, 'cancelHandler', cancelHandler);
+
+  page.render(hbs`{{donation/credit-card canCancel=true cancelAddCard=cancelHandler}}`);
+  page.cardCVC.fillIn(testInput.cardCVC);
+  page.cardMonth.selectOption(testInput.cardMonth);
+  page.cardNumber.fillIn(testInput.cardNumber);
+  page.cardYear.selectOption(testInput.cardYear);
+
+  assert.equal(page.cardCVC.value, testInput.cardCVC);
+  assert.equal(page.cardMonth.value, testInput.cardMonth);
+  assert.equal(page.cardNumber.value, testInput.cardNumber);
+  assert.equal(page.cardYear.value, testInput.cardYear);
+
+  page.clickCancel();
+
+  let freshDates = getFreshDates();
+  assert.equal(page.cardCVC.value, '');
+  assert.equal(page.cardMonth.value, freshDates.month);
+  assert.equal(page.cardNumber.value, '');
+  assert.equal(page.cardYear.value, freshDates.year);
 });
--- tests/pages/components/donation/credit-card.js
+++ tests/pages/components/donation/credit-card.js
@@ -1,15 +1,37 @@
-import { clickable, fillable, is, selectable, text } from 'ember-cli-page-object';
+import { clickable, fillable, is, isVisible, selectable, text, value } from 'ember-cli-page-object';
 
 export default {
   scope: '.credit-card-form',
 
-  cardCVC: fillable('[name=card-cvc]'),
-  cardNumber: fillable('[name=card-number]'),
-  cardMonth: selectable('select.month'),
-  cardYear: selectable('select.year'),
+  isCancelVisible: isVisible('.cancel-add-card a'),
 
-  clickSubmit: clickable('button'),
+  cardCVC: {
+    scope: '[name=card-cvc]',
+    fillIn: fillable(),
+    value: value()
+  },
 
-  submitButtonText: text('button'),
+  cardMonth: {
+    scope: 'select.month',
+    selectOption: selectable(),
+    value: value()
+  },
+
+  cardNumber: {
+    scope: '[name=card-number]',
+    fillIn: fillable(),
+    value: value()
+  },
+
+  cardYear: {
+    scope: 'select.year',
+    selectOption: selectable(),
+    value: value()
+  },
+
+  clickSubmit: clickable('button.submit-card'),
+  clickCancel: clickable('div.cancel-add-card a'),
+
+  submitButtonText: text('button.submit-card'),
   submitDisabled: is(':disabled', 'button')
 };
