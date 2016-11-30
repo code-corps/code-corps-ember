947076e87fc8d8ac6107b929f5f191be913c924e
Alex Wilkinson <alex.wilkinson@me.com>
2016-11-29 23:51:58 -0500

    (wip)

--- app/routes/project/donate.js
+++ app/routes/project/donate.js
@@ -2,7 +2,20 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import Ember from 'ember';
 
 const {
+  computed: { alias },
+  inject: { service },
   Route
 } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, {});
+export default Route.extend(AuthenticatedRouteMixin, {
+  currentUser: service(),
+  user: alias('currentUser.user'),
+  project: alias('model'),
+
+  afterModel() {
+    let subscriptions = this.get('user.stripeConnectSubscriptions');
+    let subscribedToProject = subscriptions.find(function(subscription) {
+      return subscription.get('stripeConnectPlan.project') == this.get('project');
+    });
+  }
+});
