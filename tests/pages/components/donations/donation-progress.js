5afd249f32cdaf36250372832c6553094cfc912a
Josh Smith <joshdotsmith@gmail.com>
2016-11-29 21:54:24 -0800

    Improve donation progress styles

--- tests/pages/components/donations/donation-progress.js
+++ tests/pages/components/donations/donation-progress.js
@@ -4,7 +4,7 @@ export default {
   scope: '.donation-progress',
 
   amountValue: text('.amount .value'),
-  goalDescription: text('p'),
+  goalDescription: text('.donation-progress__description'),
   percentageDescription: text('.percentage .description'),
   percentageValue: text('.percentage .value'),
   rendersProgressBar: isVisible('.progress-bar')
