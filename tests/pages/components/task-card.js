e9007699d381ebeb4e8d8a830e4e1b370edbffaf
Marine Boudeau <m14868@gmail.com>
2016-12-11 16:37:44 -0500

    Add task card model, and tests (by Josh).

---
+++ tests/pages/components/task-card.js
@@ -0,0 +1,22 @@
+import {
+  hasClass,
+  text
+} from 'ember-cli-page-object';
+
+export default {
+  scope: '.task-card',
+
+  isIdea: hasClass('task-card--idea'),
+  isIssue: hasClass('task-card--issue'),
+  isTask: hasClass('task-card--task'),
+
+  number: {
+    scope: '[data-test-selector="task number"]'
+  },
+  time: {
+    scope: '[data-test-selector="task time"]'
+  },
+  title: {
+    scope: '[data-test-selector="task title"]'
+  }
+};
