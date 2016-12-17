19970b816433f6f979e735e38c44e3cfd4411aa4
Nikola Begedin <begedinnikola@gmail.com>
2016-12-17 16:31:46 +0100

    Rework funds recipient component into a wrapper that renders appropriate subcomponents based on status

--- app/components/payments/funds-recipient.js
+++ app/components/payments/funds-recipient.js
@@ -1,40 +1,18 @@
 import Ember from 'ember';
 
 const {
-  Component, get, set
+  Component,
+  computed,
+  get
 } = Ember;
 
 export default Component.extend({
-  tagName: 'form',
+  classNameBindings: ['statusClass'],
   classNames: ['funds-recipient'],
 
-  init() {
-    this.setProperties(get(this, 'fundsRecipient'));
-    set(this, 'dobDay', 1);
-    set(this, 'dobMonth', 1);
-    set(this, 'dobYear', 2016);
-    set(this, 'country', 'US');
-    this._super(...arguments);
-  },
+  status: computed.alias('account.recipientStatus'),
 
-  actions: {
-    setBusinessType(value) {
-      set(this, 'businessType', value);
-    },
-
-    setRecipientType(value) {
-      set(this, 'recipientType', value);
-    },
-
-    submit() {
-      let fundsRecipient = this.getProperties(
-        'recipientType',
-        'businessType', 'businessName', 'businessEin',
-        'firstName', 'lastName', 'ssnLast4',
-        'dobDay', 'dobMonth', 'dobYear',
-        'address1', 'address2', 'city', 'state', 'country', 'zip'
-      );
-      this.sendAction('recipientInformationSubmitted', fundsRecipient);
-    }
-  }
+  statusClass: computed('status', function() {
+    return `account-setup__section--${get(this, 'status')}`;
+  })
 });
--- tests/integration/components/payments/funds-recipient-test.js
+++ tests/integration/components/payments/funds-recipient-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import PageObject from 'ember-cli-page-object';
@@ -6,9 +7,29 @@ import fundsRecipientComponent from '../../../pages/components/payments/funds-re
 
 let page = PageObject.create(fundsRecipientComponent);
 
+const {
+  setProperties,
+  K
+} = Ember;
+
+function setHandlers(context, { detailsHandler = K, documentHandler = K, idHandler = K }) {
+  setProperties(context, { detailsHandler, documentHandler, idHandler });
+}
+
+function renderPage() {
+  page.render(
+    hbs`{{payments/funds-recipient
+          account=account
+          onRecipientDetailsSubmitted=detailsHandler
+          onVerificationDocumentSubmitted=documentHandler
+          onPersonalIdNumberSubmitted=idHandler}}`
+  );
+}
+
 moduleForComponent('payments/funds-recipient', 'Integration | Component | payments/funds recipient', {
   integration: true,
   beforeEach() {
+    setHandlers(this, {});
     page.setContext(this);
   },
   afterEach() {
@@ -16,11 +37,99 @@ moduleForComponent('payments/funds-recipient', 'Integration | Component | paymen
   }
 });
 
-test('it renders', function(assert) {
-  page.render(hbs`{{payments/funds-recipient}}`);
-  assert.equal(this.$('.funds-recipient').length, 1, 'Component renders');
+test('it renders correctly when "pending"', function(assert) {
+  assert.expect(1);
+
+  let account = { recipientStatus: 'pending_requirement' };
+  this.set('account', account);
+
+  renderPage();
+
+  assert.ok(page.rendersPending, 'Component is rendered in pending mode.');
 });
 
-// TODO: Write tests, remove 'it renders' test
+test('it renders correctly when "required"', function(assert) {
+  assert.expect(2);
 
-// test('it sends out recipient info parameters on submit')
+  let account = { recipientStatus: 'required' };
+  this.set('account', account);
+
+  renderPage();
+
+  assert.ok(page.rendersRequired, 'Component is rendered in required mode.');
+  assert.ok(page.rendersDetailsForm, 'Component renders the details form subcomponent.');
+});
+
+test('it renders correctly when "verifying"', function(assert) {
+  assert.expect(3);
+
+  let account = { recipientStatus: 'verifying' };
+  this.set('account', account);
+
+  renderPage();
+
+  assert.ok(page.rendersVerifying, 'Component is rendered in verifying mode.');
+  assert.ok(page.rendersVerificationDocument, 'Component renders the verification document subcomponent.');
+  assert.ok(page.rendersPersonalIdNumber, 'Component renders the personal id number subcomponent.');
+});
+
+test('it renders correctly when "verified"', function(assert) {
+  assert.expect(2);
+
+  let account = {
+    recipientStatus: 'verified',
+    individualName: 'Joe Individual'
+  };
+  this.set('account', account);
+
+  renderPage();
+
+  assert.ok(page.rendersVerified, 'Component is rendered in verified mode.');
+  assert.equal(page.individualNameText, 'Joe Individual', 'Component renders the name of the registered individual.');
+});
+
+test('it renders correctly when "verified" for business', function(assert) {
+  assert.expect(3);
+
+  let account = {
+    recipientStatus: 'verified',
+    individualName: 'Joe Individual',
+    businessName: 'Company Inc.',
+    recipientType: 'business'
+  };
+  this.set('account', account);
+
+  renderPage();
+
+  assert.ok(page.rendersVerified, 'Component is rendered in verified mode.');
+  assert.ok(page.individualNameText, 'Joe Individual', 'Component renders the name of the registered individual.');
+  assert.ok(page.businessNameText, 'Company Inc.', 'Component renders the name of the registered business.');
+});
+
+// TODO: These need to be implemented once subcomponents are done and pass out actions
+// test('it passes out submit action from details subcomponent', function(assert) {
+//   assert.expect(1);
+
+//   let account = { recipientStatus: 'required' };
+//   this.set('account', account);
+
+//   renderPage();
+// });
+
+// test('it passes out submit action from document upload subcomponent', function(assert) {
+//   assert.expect(1);
+
+//   let account = { recipientStatus: 'verifying' };
+//   this.set('account', account);
+
+//   renderPage();
+// });
+
+// test('it passes out submit action from personal id number subcomponent', function(assert) {
+//   assert.expect(1);
+
+//   let account = { recipientStatus: 'verifying' };
+//   this.set('account', account);
+
+//   renderPage();
+// });
