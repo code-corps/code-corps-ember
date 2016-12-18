f76e499868b6cfa939bee6a5d12a2b78f9831ceb
Bill Heaton <pixelhandler@gmail.com>
2016-12-18 07:52:09 -0800

    WIP fixup! add component js

---
+++ app/components/payments/funds-recipient/verification-document.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const {
+  Component
+} = Ember;
+
+export default Component.extend({
+  // tagName: 'funds-recipient classNames: ['funds-recipient'],
+
+  /*
+    @property model
+    @type StripeConnectAccount
+  */
+  model: null
+});
