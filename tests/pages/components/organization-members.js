1fe1a7b9a21d8c84f50fe55f6ade04a17cfdc942
Josh Smith <joshdotsmith@gmail.com>
2017-02-05 00:15:32 -0800

    Add page object for organization-members

---
+++ tests/pages/components/organization-members.js
@@ -0,0 +1,21 @@
+import {
+  collection,
+  count,
+  isVisible,
+  property
+} from 'ember-cli-page-object';
+
+export default {
+  scope: '.organization-members',
+
+  memberCount: count('li'),
+
+  members: collection({
+    itemScope: 'li',
+    item: {
+      imageSource: property('src', 'img'),
+      imageIsVisible: isVisible('img'),
+      placeholderIsVisible: isVisible('div.icon')
+    }
+  })
+};
