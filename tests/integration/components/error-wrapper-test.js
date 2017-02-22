1440552865f2bbb5ac43900cb20822e6f66ac2b9
Josh Smith <joshdotsmith@gmail.com>
2017-02-21 21:59:16 -0800

    Add error-wrapper page object

--- tests/integration/components/error-wrapper-test.js
+++ tests/integration/components/error-wrapper-test.js
@@ -1,13 +1,18 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import PageObject from 'ember-cli-page-object';
+import component from 'code-corps-ember/tests/pages/components/error-wrapper';
+
+let page = PageObject.create(component);
 
 moduleForComponent('error-wrapper', 'Integration | Component | error wrapper', {
-  integration: true
-});
-
-test('it renders', function(assert) {
-  this.render(hbs`{{error-wrapper}}`);
-  assert.equal(this.$('.error-wrapper').length, 1, 'The component element is rendered');
+  integration: true,
+  beforeEach() {
+    page.setContext(this);
+  },
+  afterEach() {
+    page.removeContext();
+  }
 });
 
 test('it renders all required elements for the 404 case', function(assert) {
@@ -20,12 +25,12 @@ test('it renders all required elements for the 404 case', function(assert) {
   };
 
   this.set('model', error);
-  this.render(hbs`{{error-wrapper error=model}}`);
+  page.render(hbs`{{error-wrapper error=model}}`);
 
-  assert.equal(this.$('.not-found-img').length, 1, 'The 404 image renders');
-  assert.equal(this.$('h1').text().trim(), '404 Error', 'The title renders');
-  assert.equal(this.$('p:first').text().trim(), "We can't find the page you're looking for.", 'The body renders');
-  assert.equal(this.$('a.button').text().trim(), 'Go Home', 'The button renders');
+  assert.ok(page.has404Image, 'The 404 image renders');
+  assert.equal(page.title, '404 Error', 'The title renders');
+  assert.equal(page.body, "We can't find the page you're looking for.", 'The body renders');
+  assert.equal(page.button.text, 'Go Home', 'The button renders');
   assert.ok($('html').hasClass('warning'), 'The html element has the right class');
   assert.notEqual($('html').css('background-color'), 'rgba(0, 0, 0, 0)', 'The html element does not have a white background');
 });
@@ -40,12 +45,12 @@ test('it renders all required elements for the general error case', function(ass
   };
 
   this.set('model', error);
-  this.render(hbs`{{error-wrapper error=model}}`);
+  page.render(hbs`{{error-wrapper error=model}}`);
 
-  assert.equal(this.$('.server-error-img').length, 1, 'The general error image renders');
-  assert.equal(this.$('h1').text().trim(), 'Server Error', 'The title renders');
-  assert.equal(this.$('p:first').text().trim(), 'Something went wrong. Try again and if the problem persists, please report your problem and mention what caused it.', 'The body renders');
-  assert.equal(this.$('a.button').text().trim(), 'Go Home', 'The button renders');
+  assert.ok(page.hasServerErrorImage, 'The general error image renders');
+  assert.equal(page.title, 'Server Error', 'The title renders');
+  assert.equal(page.body, 'Something went wrong. Try again and if the problem persists, please report your problem and mention what caused it.', 'The body renders');
+  assert.equal(page.button.text, 'Go Home', 'The button renders');
   assert.ok($('html').hasClass('danger'), 'The html element has the right class');
   assert.notEqual($('html').css('background-color'), 'rgba(0, 0, 0, 0)', 'The html element does not have a white background');
 });
--- tests/pages/components/error-wrapper.js
+++ tests/pages/components/error-wrapper.js
@@ -1,15 +1,23 @@
 import {
   clickable,
+  isVisible,
   text
 } from 'ember-cli-page-object';
 
 export default {
   scope: '.error-wrapper',
 
+  body: text('p:first'),
+
+  button: {
+    scope: 'a.button',
+    text: text()
+  },
+
   clickLink: clickable('a'),
 
-  title: {
-    scope: 'h1',
-    text: text()
-  }
+  has404Image: isVisible('.not-found-img'),
+  hasServerErrorImage: isVisible('.server-error-img'),
+
+  title: text('h1')
 };
