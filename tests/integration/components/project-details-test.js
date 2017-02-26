29822a32990766b25bb1fb158b77913ceac4b080
Josh Smith <joshdotsmith@gmail.com>
2017-02-26 15:11:06 -0800

    Fixes #738 with project-details page object

--- tests/integration/components/project-details-test.js
+++ tests/integration/components/project-details-test.js
@@ -1,15 +1,32 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import PageObject from 'ember-cli-page-object';
+import component from 'code-corps-ember/tests/pages/components/project-details';
+
+let page = PageObject.create(component);
 
 moduleForComponent('project-details', 'Integration | Component | project details', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    page.setContext(this);
+  },
+  afterEach() {
+    page.removeContext();
+  }
 });
 
+let project = {
+  title: 'Test Project',
+  iconThumbUrl: 'icon.png'
+};
+
 test('it renders title', function(assert) {
-  let project = { title: 'Test title' };
+  assert.expect(2);
+
   this.set('project', project);
 
-  this.render(hbs`{{project-details project=project}}`);
+  page.render(hbs`{{project-details project=project}}`);
 
-  assert.equal(this.$('.project__header__title').text().trim(), project.title, 'Title is rendered');
+  assert.equal(page.icon.src, project.iconThumbUrl, 'Icon is rendered');
+  assert.equal(page.title.text, project.title, 'Title is rendered');
 });
