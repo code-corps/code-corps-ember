7a4df2857df35dbb97ed6b89651d91c0606c768f
Nikola Begedin <begedinnikola@gmail.com>
2017-04-10 15:32:35 +0200

    Fix run loop issues for role item and user dropdown integration tests

--- app/components/role-item.js
+++ app/components/role-item.js
@@ -5,7 +5,9 @@ const {
   computed,
   computed: { notEmpty },
   get,
-  inject: { service }
+  getProperties,
+  inject: { service },
+  set
 } = Ember;
 
 export default Component.extend({
@@ -19,31 +21,30 @@ export default Component.extend({
   selected: notEmpty('userRole'),
 
   userRole: computed('role', 'userRoles.userRoles', function() {
-    let role = this.get('role');
-    let userRoles = this.get('userRoles');
+    let { role, userRoles } = getProperties(this, 'role', 'userRoles');
     return userRoles.findUserRole(role);
   }),
 
   actions: {
     addRole(role) {
-      this.set('isLoading', true);
+      set(this, 'isLoading', true);
       let userRoles = this.get('userRoles');
       return userRoles.addRole(role).catch(() => {
         let message = `An error occurred trying to add ${role.get('name')}.`;
         this._flashError(message);
       }).finally(() => {
-        this.set('isLoading', false);
+        set(this, 'isLoading', false);
       });
     },
 
     removeRole(role) {
-      this.set('isLoading', true);
+      set(this, 'isLoading', true);
       let userRoles = this.get('userRoles');
       return userRoles.removeRole(role).catch(() => {
         let message = `An error occurred trying to remove ${role.get('name')}.`;
         this._flashError(message);
       }).finally(() => {
-        this.set('isLoading', false);
+        set(this, 'isLoading', false);
       });
     }
   },
