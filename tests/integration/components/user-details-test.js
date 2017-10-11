c5339b7e3188543be484bb3fdfe09e5cd65071ea
Josh Smith <josh@codecorps.org>
2017-10-11 16:46:42 -0700

    Add the user's skills to their profile page

--- tests/integration/components/user-details-test.js
+++ tests/integration/components/user-details-test.js
@@ -2,6 +2,11 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import PageObject from 'ember-cli-page-object';
 import component from 'code-corps-ember/tests/pages/components/user-details';
+import Ember from 'ember';
+
+const {
+  set
+} = Ember;
 
 let page = PageObject.create(component);
 
@@ -22,4 +27,36 @@ test('it renders subcomponents', function(assert) {
 
   assert.ok(page.userProjectsList.isVisible);
   assert.ok(page.userSidebar.isVisible);
+});
+
+test('it renders the user skills list when the user has skills', function(assert) {
+  assert.expect(2);
+
+  let user = {
+    userSkills: [
+      {
+        skill: {
+          title: 'Ember.js'
+        }
+      }
+    ]
+  };
+  set(this, 'user', user);
+
+  page.render(hbs`{{user-details user=user}}`);
+
+  assert.ok(page.userSkillsList.isVisible);
+  assert.equal(page.userSkillsList.skills(0).text, 'Ember.js');
+});
+
+test('it renders the empty state when the user has no skills', function(assert) {
+  assert.expect(2);
+
+  let user = { userSkills: [] };
+  set(this, 'user', user);
+
+  page.render(hbs`{{user-details user=user}}`);
+
+  assert.notOk(page.userSkillsList.isVisible);
+  assert.ok(page.userSkillsListEmptyState.isVisible);
 });
