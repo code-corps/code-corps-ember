05ef8569043f2234e9d5954dfb4c8a3f0177308a
Dave Connis <dave@codecorps.org>
2017-11-14 12:02:10 -0500

    mouseEnter dropdownOpen tests

--- tests/unit/mixins/tooltip-for-dropdown-test.js
+++ tests/unit/mixins/tooltip-for-dropdown-test.js
@@ -1,6 +1,7 @@
 import EmberObject from '@ember/object';
 import TooltipForDropdownMixin from 'code-corps-ember/mixins/tooltip-for-dropdown';
 import { module, test } from 'qunit';
+import { set } from '@ember/object';
 
 module('Unit | Mixin | tooltip for dropdown');
 
@@ -9,4 +10,18 @@ test('it works', function(assert) {
   let TooltipForDropdownObject = EmberObject.extend(TooltipForDropdownMixin);
   let subject = TooltipForDropdownObject.create();
   assert.ok(subject);
+});
+
+test('mouseEnter dropdownOpen is true', function(assert) {
+  let TooltipForDropdownObject = EmberObject.extend(TooltipForDropdownMixin);
+  TooltipForDropdownObject.create();
+  set(this, 'dropdownOpen', true);
+  assert.equal(this.dropdownOpen, true);
+});
+
+test('mouseEnter dropdownOpen is false', function(assert) {
+  let TooltipForDropdownObject = EmberObject.extend(TooltipForDropdownMixin);
+  TooltipForDropdownObject.create();
+  set(this, 'dropdownOpen', false);
+  assert.equal(this.dropdownOpen, false);
 });
