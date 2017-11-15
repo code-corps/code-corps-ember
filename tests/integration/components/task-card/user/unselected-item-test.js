e450cd4e66117af7e03c244e97c7bd8b9985d344
Dave Connis <dave@codecorps.org>
2017-11-15 16:02:06 -0500

    wrote 'mouseEnter hovers and tooltipShown' test

--- tests/integration/components/task-card/user/unselected-item-test.js
+++ tests/integration/components/task-card/user/unselected-item-test.js
@@ -10,19 +10,33 @@ function renderPage() {
   page.render(hbs`
     {{unselected-item
       dropdownOpen=dropdownOpen
-      tooltipShown=tooltipShown 
-     }}`);
+      tooltipShown=tooltipShown
+     }}
+     `);
 }
 
+let dropdownOpen = false;
+let tooltipShown = false;
+
 moduleForComponent('task-card/user/unselected-item', 'Integration | Component | task card/user/unselected item', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    page.setContext(this);
+  },
+  afterEach() {
+    page.removeContext();
+  }
 });
 
-test('mouseEnter hovers', function(assert) {
+test('mouseEnter hovers and tooltipShown', function(assert) {
+  set(this, 'dropdownOpen', dropdownOpen);
+  set(this, 'tooltipShown', tooltipShown);
   renderPage();
 
-
-
+  assert.ok(page.unselectedIcon.isVisable, 'the user icon renders');
+  page.mouseenter(); // trigger the tooltip
+  assert.ok(page.isTooltipTarget, 'There is a tooltip.');
+  assert.equal(page.tooltip.text, 'Assign this task', 'The tooltip renders with text.');
+  page.mouseleave();// hide the tooltip
+  assert.ok(page.tooltip.isHidden);
 });
-
-test('mouseLeave unhovers')
