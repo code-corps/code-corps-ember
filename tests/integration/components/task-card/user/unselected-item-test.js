018599e6fd9e517cb9e7b2d8fef97d1fcad9bcb4
Dave Connis <dave@codecorps.org>
2017-11-16 16:30:46 -0500

    rewrote tests, added default state tests

--- tests/integration/components/task-card/user/unselected-item-test.js
+++ tests/integration/components/task-card/user/unselected-item-test.js
@@ -1,22 +1,15 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import PageObject from 'ember-cli-page-object';
-import { set } from '@ember/object';
-import unselectedItemComponent from 'code-corps-ember/tests/pages/unselected-item.js';
+import unselectedItemComponent from 'code-corps-ember/tests/pages/components/unselected-item';
 
 let page = PageObject.create(unselectedItemComponent);
 
 function renderPage() {
   page.render(hbs`
-    {{unselected-item
-      dropdownOpen=dropdownOpen
-      tooltipShown=tooltipShown
-     }}
-     `);
+    {{task-card/user/unselected-item task=task}}
+  `);
 }
-
-let dropdownOpen = false;
-let tooltipShown = false;
 
 moduleForComponent('task-card/user/unselected-item', 'Integration | Component | task card/user/unselected item', {
   integration: true,
@@ -28,16 +21,29 @@ moduleForComponent('task-card/user/unselected-item', 'Integration | Component | 
   }
 });
 
-test('mouseEnter hovers and tooltipShown', function(assert) {
-  set(this, 'dropdownOpen', dropdownOpen);
-  set(this, 'tooltipShown', tooltipShown);
+test('the default state when user task is loading', function(assert) {
+  let task = { userTask: { isLoading: true } };
+  this.set('task', task);
   renderPage();
-
-  assert.ok(page.unselectedIcon.isVisable, 'the user icon renders');
-  page.mouseenter(); // trigger the tooltip
-  assert.ok(page.isTooltipTarget, 'There is a tooltip.');
-  assert.equal(page.dropdownOpen, false, 'Dropdown is not open.'),
-  assert.equal(page.tooltip.text, 'Assign this task', 'The tooltip renders with text.');
-  page.mouseleave();// hide the tooltip
-  assert.ok(page.tooltip.isHidden);
+  assert.notOk(page.isTooltipTarget, 'There is not a tooltip.');
+  assert.ok(page.loadingIcon.isVisible, 'The loading icon renders.');
 });
+
+test('the default state when user task is loaded', function(assert) {
+  let task = { userTask: { isLoading: false } };
+  this.set('task', task);
+  renderPage();
+  assert.notOk(page.loadingIcon.isVisible, 'The loading icon does not render.');
+  assert.notOk(page.isTooltipTarget, 'There is no tooltip because it lazy renders.');
+  // assert.ok(page.tooltip.isHidden);
+});
+
+// test('when hovering', function(assert) {
+//
+//   renderPage();
+//   assert.ok(page.isTooltipTarget, 'There is a tooltip.');
+//   assert.equal(page.dropdownOpen, false, 'Dropdown is not open.'),
+//   assert.equal(page.tooltip.text, 'Assign this task', 'The tooltip renders with text.');
+//   page.mouseleave();// hide the tooltip
+//   assert.ok(page.tooltip.isHidden);
+// });
--- tests/pages/components/unselected-item.js
+++ tests/pages/components/unselected-item.js
@@ -1,17 +1,17 @@
 import { triggerable, hasClass } from 'ember-cli-page-object';
 import tooltip from 'code-corps-ember/tests/pages/helpers/tooltip';
 
-export default{
-
-  scope: '.unselected-item',
+export default {
+  scope: '.select-inline__unselected-item',
 
   isTooltipTarget: hasClass('ember-tooltip-or-popover-target'),
+
+  loadingIcon: {
+    scope: '[data-test-loading-icon]'
+  },
 
   mouseenter: triggerable('mouseenter'),
   mouseleave: triggerable('mouseleave'),
 
-  unselectedIcon: 'data-test-unselected-icon',
-
   tooltip
-
 };
