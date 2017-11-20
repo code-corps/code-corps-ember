4d64f548412cce7d027f8823a1b65ba1c9e36ce8
Dave Connis <dave@codecorps.org>
2017-11-20 16:01:53 -0500

    added mouseEnter test

--- tests/integration/components/task-card/user/unselected-item-test.js
+++ tests/integration/components/task-card/user/unselected-item-test.js
@@ -1,14 +1,22 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import PageObject from 'ember-cli-page-object';
-import unselectedItemComponent from 'code-corps-ember/tests/pages/components/unselected-item';
+import {
+  assertTooltipRendered,
+  triggerTooltipTargetEvent
+} from 'code-corps-ember/tests/helpers/ember-tooltips';
+import unselectedItem from 'code-corps-ember/tests/pages/components/task-card/user/unselected-item';
 
-let page = PageObject.create(unselectedItemComponent);
+let page = PageObject.create(unselectedItem);
 
 function renderPage() {
   page.render(hbs`
-    {{task-card/user/unselected-item task=task}}
-  `);
+    {{task-card/user/unselected-item
+      task=task
+      select-inline__unselected-item__icon= 'has-assign-tooltip'}}
+
+     {{tooltip-on-element}}
+   `);
 }
 
 moduleForComponent('task-card/user/unselected-item', 'Integration | Component | task card/user/unselected item', {
@@ -35,7 +43,29 @@ test('the default state when user task is loaded', function(assert) {
   renderPage();
   assert.notOk(page.loadingIcon.isVisible, 'The loading icon does not render.');
   assert.notOk(page.isTooltipTarget, 'There is no tooltip because it lazy renders.');
-  // assert.ok(page.tooltip.isHidden);
+});
+
+// Tooltip won't be rendered in the DOM yet because enableLazyRendering delays the rendering until the user interacts with the target.
+
+test('mouseEnter enters', function(assert) {
+  renderPage();
+  triggerTooltipTargetEvent($(this), 'mouseenter');
+  assertTooltipRendered(assert);
+});
+
+test('mouseLeave leaves', function(assert) {
+  assert.expect(1);
+  let mouseLeave = function() {
+    this.triggerAction({
+      action: 'mouseLeave',
+      target: this.get('tooltipShown')
+    });
+
+    renderPage();
+    triggerTooltipTargetEvent($(this), 'mouseenter');
+    mouseLeave();
+    assert.equal(page.tooltipShown, 'false', 'there is no tooltip');
+  };
 });
 
 // test('when hovering', function(assert) {
