3364cd4adb98c292311490786398489f562afbd0
daveconnis <dave.connis@gmail.com>
2017-12-12 15:58:50 -0500

    Add slugged-route-model-details page object

---
+++ tests/pages/components/slugged-route-model-details.js
@@ -0,0 +1,9 @@
+import organizationProfile from 'code-corps-ember/tests/pages/components/organization-profile';
+import userDetails from 'code-corps-ember/tests/pages/components/user-details';
+
+export default {
+  scope: '.slugged-route-model-details',
+
+  organizationProfile,
+  userDetails
+};
