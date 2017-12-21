b0972dbeba37f3ae63f7155eb7e92e0c3a205098
Nikola Begedin <begedinnikola@gmail.com>
2017-12-21 11:09:22 +0100

    Expand existing acceptance test to cover ordering and timestamp formatting

--- tests/acceptance/user-conversations-test.js
+++ tests/acceptance/user-conversations-test.js
@@ -4,6 +4,8 @@ import moduleForAcceptance from 'code-corps-ember/tests/helpers/module-for-accep
 import { authenticateSession } from 'code-corps-ember/tests/helpers/ember-simple-auth';
 import page from 'code-corps-ember/tests/pages/conversations';
 
+const hourOfMiliseconds = 3600 * 1000;
+
 moduleForAcceptance('Acceptance | User Conversations');
 
 test('Page requires authentication', function(assert) {
@@ -17,17 +19,42 @@ test('Page requires authentication', function(assert) {
 });
 
 test('User can view list of their own conversations', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
 
   let user = server.create('user');
   authenticateSession(this.application, { user_id: user.id });
 
-  server.createList('conversation', 3, { user });
+  let [date1, date2, date3] = [
+    Date.now(),
+    Date.now() - 5 * hourOfMiliseconds,
+    Date.now() - 1 * hourOfMiliseconds
+  ];
+
+  server.create('conversation', { user, updatedAt: date1 });
+  server.create('conversation', { user, updatedAt: date2 });
+  server.create('conversation', { user, updatedAt: date3 });
 
   page.visit();
 
   andThen(() => {
     assert.equal(page.conversations().count, 3, 'Conversations are rendered');
+    let renderedTimeStampOrder = [
+      page.conversations(0).updatedAt.text,
+      page.conversations(1).updatedAt.text,
+      page.conversations(2).updatedAt.text
+    ];
+
+    let expectedTimeStampOrder = [
+      'a few seconds ago',
+      'an hour ago',
+      '5 hours ago'
+    ];
+
+    assert.deepEqual(
+      renderedTimeStampOrder,
+      expectedTimeStampOrder,
+      'Items are rendered in correct order with properly formatted timestamps.'
+    );
   });
 });
 
--- tests/pages/conversations.js
+++ tests/pages/conversations.js
@@ -4,12 +4,14 @@ import {
   visitable
 } from 'ember-cli-page-object';
 import conversationThread from 'code-corps-ember/tests/pages/components/conversations/conversation-thread';
+import conversationListItemWithProject from 'code-corps-ember/tests/pages/components/conversations/conversation-list-item-with-project';
 
 export default create({
   visit: visitable('/conversations'),
 
   conversations: collection({
-    itemScope: '.conversation-list-item'
+    itemScope: '.conversation-list-item',
+    item: conversationListItemWithProject
   }),
 
   conversationThread
