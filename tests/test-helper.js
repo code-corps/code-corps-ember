3e7dee6074073b9bb9ab3f563659c2163de83f3e
Nikola Begedin <begedinnikola@gmail.com>
2018-01-19 16:48:50 +0100

    Cleanup lint errors, update code based on latest ember-cli blueprint

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,5 @@
 import Application from '../app';
+import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import { run } from '@ember/runloop';
@@ -6,9 +7,6 @@ import loadEmberExam from 'ember-exam/test-support/load';
 import './helpers/flash-message';
 
 loadEmberExam();
-
 run.later = run.next;
-
-setApplication(Application.create({ autoboot: false }));
-
+setApplication(Application.create(config.APP));
 start();
