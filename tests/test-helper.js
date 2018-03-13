6570f3a5cd05c08dd1b6a9f775ca6dcaecaa7a73
John Derr <john.derr@gmail.com>
2018-03-13 10:51:07 -0700

    Added native dom helpers support

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,10 +3,12 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import { run } from '@ember/runloop';
+import { useNativeEvents } from 'ember-cli-page-object/extend';
 import loadEmberExam from 'ember-exam/test-support/load';
 import './helpers/flash-message';
 
 loadEmberExam();
+useNativeEvents();
 run.later = run.next;
 setApplication(Application.create(config.APP));
 start();
